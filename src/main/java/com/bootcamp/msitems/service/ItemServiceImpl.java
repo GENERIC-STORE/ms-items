5ba29ef33e88d8e773e8524a88b1e03d2da78fa8
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-08 14:47:02 -0500

    add and use restClient bean

--- src/main/java/com/bootcamp/msitems/service/ItemServiceImpl.java
+++ src/main/java/com/bootcamp/msitems/service/ItemServiceImpl.java
@@ -1,12 +1,18 @@
 package com.bootcamp.msitems.service;
 
 import com.bootcamp.msitems.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class ItemServiceImpl implements IItemService{
+
+    @Autowired
+    private RestTemplate restClient;
+
     @Override
     public Flux<Item> findAll() {
         return null;
