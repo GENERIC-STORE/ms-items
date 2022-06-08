262b38068931dfc26d6722a9983809bea2f4fbac
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-08 14:59:30 -0500

    base implementation of service

--- src/main/java/com/bootcamp/msitems/service/ItemServiceImpl.java
+++ src/main/java/com/bootcamp/msitems/service/ItemServiceImpl.java
@@ -1,11 +1,18 @@
 package com.bootcamp.msitems.service;
 
 import com.bootcamp.msitems.model.Item;
+import com.bootcamp.msitems.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements IItemService{
@@ -15,11 +22,18 @@ public class ItemServiceImpl implements IItemService{
 
     @Override
     public Flux<Item> findAll() {
-        return null;
+        List<Product> products = Arrays.asList(restClient.getForObject("http://localhost:3000/product", Product[].class));
+
+        return Flux.fromIterable(products.stream().map(p -> new Item(p, 1)).collect(Collectors.toList()));
     }
 
     @Override
     public Mono<Item> findById(Long id, Integer quantity) {
-        return null;
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("id", id.toString());
+
+        Product product = restClient.getForObject("http://localhost:3000/product", Product.class, pathVariables);
+
+        return Mono.just(new Item(product, quantity));
     }
 }
